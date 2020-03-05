eef758dc68d9e5b2e7e8f45127e1616ee445c550
jensberger90 <51542562+jensberger90@users.noreply.github.com>
2020-03-06 00:08:56 +0100

    multiple Players

--- app.js
+++ app.js
@@ -10,15 +10,36 @@ app.use('/client' ,express.static(__dirname + '/client'));
 serv.listen(2000);
 console.log('Server started.')
 
+var SOCKET_LIST = {};
+
 var io = require('socket.io') (serv, {});
 io.sockets.on('connection', function(socket) {
-    console.log('socket connection');
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
 
-    socket.on('happy', function(data) {
-        console.log(data.reason);
-    });
+    SOCKET_LIST[socket.id] = socket;
 
-    socket.emit('serverMsg', {
-        msg:'hello',
-    });
 });
+
+setInterval(function() {
+    var pack = [];
+    for (var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        pack.push({
+            x:socket.x,
+            y:socket.y
+        })
+
+    }
+
+
+    for (var i in SOCKET_LIST) {
+        socket.emit('newPositions',pack);
+    }
+
+
+
+}, 1000/25);

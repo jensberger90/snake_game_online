0abc542c14e6be7e80092dd7d07af9977bbb9666
jensberger90 <51542562+jensberger90@users.noreply.github.com>
2020-03-06 23:54:01 +0100

    Schieße

--- app.js
+++ app.js
@@ -11,61 +11,76 @@ serv.listen(2000);
 console.log("Server started.");
  
 var SOCKET_LIST = {};
-var POINT_WIDTH = 10;
+var POINT_WIDTH = 20;
+var MAX_LENGTH = 50*50;
 
-var Entity = function(){
-    var self = {
-        x:250,
-        y:250,
-        spdX:0,
-        spdY:0,
-        id:"",
-    }
-    self.update = function(){
-        self.updatePosition();
-    }
-    self.updatePosition = function(){
-        self.x += self.spdX;
-        self.y += self.spdY;
-    }
-    self.getDistance = function(pt){
-        return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
-    }
-    return self;
-}
- 
+
 var Player = function(id){
-    var self = Entity();
-    self.id = id;
-    self.number = "" + Math.floor(10 * Math.random());
-    self.pressingRight = false;
-    self.pressingLeft = false;
-    self.pressingUp = false;
-    self.pressingDown = false;
-    self.mouseAngle = 0;
-    self.maxSpd = 10;
-   
-    var super_update = self.update;
+    var self = {
+        id:id,
+        number:"" + Math.floor(10 * Math.random()),
+        moveright:false,
+        moveleft:false,
+        moveup:false,
+        x:10,
+        y:10,
+        movedown:false,
+        maxSpd:1,
+        points:1,
+        
+        
+    }
+
+    self.pointx = new Array(self.points);
+    self.pointy = new Array(self.points);
+
+    for (var i in self.pointx) {
+        self.pointx[i] = self.x - i;
+        self.pointy[i] = self.y;
+    }
+
     self.update = function(){
+        if(self.x > 500)
+            self.x = 0;
+        else if(self.x < 0)
+            self.x = 500;
+
+        if(self.y > 500)
+            self.y = 0;
+        else if(self.y < 0)
+            self.y = 500;
+
+            self.x += self.spdX;
+            self.y += self.spdY;
+
         self.updateSpd();
-        super_update();
+
+
+
+        for (var i = self.points; i > 1; i--) {
+            self.pointx[i] =  self.pointx[i-1];
+            self.pointy[i] =  self.pointy[i-1];
+        }
+
+        self.pointx[0] = self.x;
+        self.pointy[0] = self.y;
     }
 
    
     self.updateSpd = function(){
-        if(self.pressingRight)
-            self.spdX = self.maxSpd;
-        else if(self.pressingLeft)
+        self.spdY = 0;
+        self.spdX = 0;
+        
+        if(self.moveright)
+            self.spdX = self.maxSpd;   
+        else if(self.moveleft)
             self.spdX = -self.maxSpd;
-        else
-            self.spdX = 0;
-       
-        if(self.pressingUp)
+        else if(self.moveup)
             self.spdY = -self.maxSpd;
-        else if(self.pressingDown)
+        else if(self.movedown)
             self.spdY = self.maxSpd;
-        else
-            self.spdY = 0;     
+
+ 
     }
     Player.list[id] = self;
     return self;
@@ -73,17 +88,21 @@ var Player = function(id){
 Player.list = {};
 Player.onConnect = function(socket){
     var player = Player(socket.id);
+
     socket.on('keyPress',function(data){
+        player.moveleft = false;
+        player.movedown = false;
+        player.moveright = false;
+        player.moveup = false;
+
         if(data.inputId === 'left')
-            player.pressingLeft = data.state;
+            player.moveleft = true;
         else if(data.inputId === 'right')
-            player.pressingRight = data.state;
+            player.moveright = true;
         else if(data.inputId === 'up')
-            player.pressingUp = data.state;
+            player.moveup = true;
         else if(data.inputId === 'down')
-            player.pressingDown = data.state;
-        else if(data.inputId === 'mouseAngle')
-            player.mouseAngle = data.state;
+            player.movedown = true;
     });
 }
 Player.onDisconnect = function(socket){
@@ -95,9 +114,8 @@ Player.update = function(){
         var player = Player.list[i];
         player.update();
         pack.push({
-            x:player.x,
-            y:player.y,
-            number:player.number
+            pointx:player.pointx,
+            pointy:player.pointy,
         });    
     }
     return pack;
@@ -112,10 +130,13 @@ io.sockets.on('connection', function(socket){
     SOCKET_LIST[socket.id] = socket;
    
 
+
+
     socket.on('disconnect',function(){
         delete SOCKET_LIST[socket.id];
         Player.onDisconnect(socket);
     });
+
     socket.on('sendMsgToServer',function(data){
         var playerName = ("" + socket.id).slice(2,7);
         for(var i in SOCKET_LIST){
@@ -145,4 +166,4 @@ setInterval(function(){
         var socket = SOCKET_LIST[i];
         socket.emit('newPositions',pack);
     }
-},1000/25);
+},100);

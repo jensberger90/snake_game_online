65867c2abbab47fd6565de7f76755178e143eb3d
jensberger90 <51542562+jensberger90@users.noreply.github.com>
2020-03-20 21:55:26 +0100

    commit master

--- app.js
+++ app.js
@@ -9,157 +9,171 @@ app.use('/client',express.static(__dirname + '/client'));
  
 serv.listen(2000);
 console.log("Server started.");
- 
+
+
+var PLAYER_LIST = {}; 
 var SOCKET_LIST = {};
-var POINT_WIDTH = 20;
-var MAX_LENGTH = 50*50;
+var WIDTH = 500;
+var HEIGHT = 500;
+var pq = 25;
+
+var px = getRandomInt(1, (WIDTH/pq)-1); 
+var py = getRandomInt(1, (HEIGHT/pq)-1);
+
+function Player(id)
+{
+    this.id = id;
+    this.x = 15;
+    this.y = 12;
+    this.xplus = 1;
+    this.yplus = 0;
+    this.snakex = [];
+    this.snakey = [];
+    this.laenge = 5;
+    this.score = 0;
 
 
-var Player = function(id){
-    var self = {
-        id:id,
-        number:"" + Math.floor(10 * Math.random()),
-        moveright:false,
-        moveleft:false,
-        moveup:false,
-        x:10,
-        y:10,
-        movedown:false,
-        maxSpd:1,
-        points:1,
-        
-        
+
+    this.initialize = function() {
+        for(var i = 0; i < this.laenge-1; i++){
+            this.snakex[i] = this.x - i;
+            this.snakey[i] = this.y;
+        }
     }
 
-    self.pointx = new Array(self.points);
-    self.pointy = new Array(self.points);
+    this.update = function() {
+        this.x += this.xplus;
+        this.y += this.yplus;
+        
+        if(this.x > (WIDTH/pq)-1)
+            this.x = 0;
+        else if(this.x < 0)
+            this.x =  (WIDTH/pq)-1;
+        else if(this.y > (HEIGHT/pq)-1)
+            this.y = 0;
+        else if(this.y < 0)
+            this.y = (HEIGHT/pq)-1;
 
-    for (var i in self.pointx) {
-        self.pointx[i] = self.x - i;
-        self.pointy[i] = self.y;
-    }
-
-    self.update = function(){
-        if(self.x > 500)
-            self.x = 0;
-        else if(self.x < 0)
-            self.x = 500;
-
-        if(self.y > 500)
-            self.y = 0;
-        else if(self.y < 0)
-            self.y = 500;
-
-            self.x += self.spdX;
-            self.y += self.spdY;
-
-        self.updateSpd();
-
-
-
-        for (var i = self.points; i > 1; i--) {
-            self.pointx[i] =  self.pointx[i-1];
-            self.pointy[i] =  self.pointy[i-1];
+        for (var i = this.laenge-1; i > 0; i--) {
+            this.snakex[i] = this.snakex[i - 1];
+            this.snakey[i] = this.snakey[i - 1];            
         }
 
-        self.pointx[0] = self.x;
-        self.pointy[0] = self.y;
-    }
+        this.snakex[0] = this.x;
+        this.snakey[0] = this.y;
 
-   
-    self.updateSpd = function(){
-        self.spdY = 0;
-        self.spdX = 0;
+
+        //kollision innerhalb der schlange
+       for (var i = 1; i < this.laenge; i++) {
+            if(this.snakex[0] == this.snakex[i] && this.snakey[0] == this.snakey[i]){
+                this.laenge = 5;
+                this.score = 0;
+            }
+        }
+
+        //punkt einsammeln
+        if(this.x == px && this.y == py){
+            this.laenge = this.laenge + 1;
+
+            //this.snakex[this.laenge] = this.snakex[this.laenge-1];
+            //this.snakey[this.laenge] = this.snakey[this.laenge-1];
+             
+
+            for (var i = this.laenge-1; i > this.laenge; i++) {
+                this.snakex[i] = this.snakex[i - 1];
+                this.snakey[i] = this.snakey[i - 1];            
+            }
+
+            //console.log("catch" + px + " "+ py);
+
+            px = getRandomInt(1, (WIDTH/pq)-1); 
+            py = getRandomInt(1, (HEIGHT/pq)-1);        }
+
         
-        if(self.moveright)
-            self.spdX = self.maxSpd;   
-        else if(self.moveleft)
-            self.spdX = -self.maxSpd;
-        else if(self.moveup)
-            self.spdY = -self.maxSpd;
-        else if(self.movedown)
-            self.spdY = self.maxSpd;
+        if(this.snakex.length > this.laenge){
+            this.snakex.splice(this.laenge-1, (this.snakex.length-this.laenge))
+            this.snakey.splice(this.laenge-1, (this.snakey.length-this.laenge))
+        }
 
- 
     }
-    Player.list[id] = self;
-    return self;
+
+
 }
-Player.list = {};
-Player.onConnect = function(socket){
-    var player = Player(socket.id);
+
+
+
+
+
+
+onConnect = function(socket){
+    var player = new Player(socket.id);
+    
+    player.initialize();
+
+    PLAYER_LIST[player.id] = player;
 
     socket.on('keyPress',function(data){
-        player.moveleft = false;
-        player.movedown = false;
-        player.moveright = false;
-        player.moveup = false;
+        if(data.inputId === 'left'){
+            player.xplus = -1;
+            player.yplus = 0;
+        }else if(data.inputId === 'right'){
+            player.xplus = 1;
+            player.yplus = 0;        
+        }
+        else if(data.inputId === 'up'){
+            player.xplus = 0;
+            player.yplus = -1;        
+        }
+        else if(data.inputId === 'down'){
+            player.xplus = 0;
+            player.yplus = 1;        
+        }
+            
+    });
 
-        if(data.inputId === 'left')
-            player.moveleft = true;
-        else if(data.inputId === 'right')
-            player.moveright = true;
-        else if(data.inputId === 'up')
-            player.moveup = true;
-        else if(data.inputId === 'down')
-            player.movedown = true;
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+        onDisconnect(socket);
     });
 }
-Player.onDisconnect = function(socket){
-    delete Player.list[socket.id];
+
+onDisconnect = function(socket){
+    delete PLAYER_LIST[socket.id];
 }
-Player.update = function(){
-    var pack = [];
-    for(var i in Player.list){
-        var player = Player.list[i];
-        player.update();
-        pack.push({
-            pointx:player.pointx,
-            pointy:player.pointy,
-        });    
-    }
-    return pack;
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
- 
-var DEBUG = true;
- 
  
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
-   
-
-
-
-    socket.on('disconnect',function(){
-        delete SOCKET_LIST[socket.id];
-        Player.onDisconnect(socket);
-    });
-
-    socket.on('sendMsgToServer',function(data){
-        var playerName = ("" + socket.id).slice(2,7);
-        for(var i in SOCKET_LIST){
-            SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
-        }
-    });
-   
-    socket.on('evalServer',function(data){
-        if(!DEBUG)
-            return;
-        var res = eval(data);
-        socket.emit('evalAnswer',res);     
-    });
-   
-    Player.onConnect(socket);
-
-   
+    onConnect(socket);
 });
 
 
 setInterval(function(){
+    var data = [];
+        for(var i in PLAYER_LIST){
+            var player = PLAYER_LIST[i];
+            player.update();
+            data.push({
+                snakex:player.snakex,
+                snakey:player.snakey,
+                score:player.laenge,
+            });    
+        }
+    
     var pack = {
-        player:Player.update(),
+        player:data,
+        goal:{
+            px,
+            py,
+        },
     }
    
     for(var i in SOCKET_LIST){
@@ -167,3 +181,4 @@ setInterval(function(){
         socket.emit('newPositions',pack);
     }
 },100);
+
